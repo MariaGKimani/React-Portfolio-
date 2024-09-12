e213a2ed95e4a1174ea7d7efee265d56ce8aa4ae
MariaGKimani <mariakimanigoretti@gmail.com>
2024-09-12 15:06:19 +0300

    feat: update project structure and replace App.css with App.scss
    - Modified package.json and package-lock.json for updated dependencies
    - Removed src/App.css and replaced with src/App.scss for better styling management
    - Updated src/App.js and src/index.js for SCSS integration and other code improvements
    - Modified src/index.css for updated global styles
    - Added new assets and components under src/assets/ and src/components/

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,53 @@
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss';
+import Logo from '../../assets/images/logo1-removebg-preview.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+
+const Sidebar = () => {
+  return (
+    <div className='navbar'>
+      <Link className='logo' to='/'>
+        <img src={Logo} alt='logo' />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#COCED1" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#COCED1" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#COCED1" />
+        </NavLink>
+      </nav>
+
+      <ul>
+        <li>
+            <a target='_blank' rel='' href='https://www.linkedin.com/in/mariagoretti-kimani-/'>
+                <FontAwesomeIcon icon={faLinkedin}/>
+            </a>
+        </li>
+        <li>
+            <a target='_blank' rel='' href='https://github.com/MariaGKimani'>
+                <FontAwesomeIcon icon={faGithub} />
+            </a>
+        </li>  
+        <li>
+            <a target='_blank' rel='' href='https://medium.com/@mariakimanigoretti'>
+                <FontAwesomeIcon icon={faMedium} />
+            </a>
+        </li>
+        <li>
+            <a target='_blank' rel='' href='https://twitter.com/MariagorettiK '>
+                <FontAwesomeIcon icon={faTwitter} />
+            </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
