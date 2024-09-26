cd594396309d56db40350d5656599e3c6cb3eb8f
MariaGKimani <mariakimanigoretti@gmail.com>
2024-09-26 15:39:13 +0300

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import Sidebar from './components/Sidebar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
